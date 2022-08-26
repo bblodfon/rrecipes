affa2324505871a25daef26c3b64728a4fd51b10
john <bblodfon@gmail.com>
2022-08-26 10:41:35 +0200

    add benchmark script

---
+++ miscellaneous/benchmark.R
@@ -0,0 +1,9 @@
+library(rbenchmark)
+
+within(rbenchmark::benchmark(
+	'test1' = { Sys.sleep(0.5) },
+        'test2' = { Sys.sleep(0.3) },
+        replications = 5,
+        columns = c("test", "replications", "elapsed", "relative")
+), { average = elapsed/replications})
+
