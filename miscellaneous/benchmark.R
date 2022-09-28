d0e8a9d4f37c76a2b78b7682571ff03f9ced159b
john <bblodfon@gmail.com>
2022-09-28 11:28:14 +0200

    better formating

--- miscellaneous/benchmark.R
+++ miscellaneous/benchmark.R
@@ -2,8 +2,7 @@ library(rbenchmark)
 
 within(rbenchmark::benchmark(
 	'test1' = { Sys.sleep(0.5) },
-        'test2' = { Sys.sleep(0.3) },
-        replications = 5,
-        columns = c("test", "replications", "elapsed", "relative")
-), { average_min = (elapsed/replications)/60})
-
+  'test2' = { Sys.sleep(0.3) },
+  replications = 5,
+  columns = c("test", "replications", "elapsed", "relative")
+), { average_min = (elapsed/replications)})
