ea72863b614657a78c8a93194e9e25abb365f958
john <bblodfon@gmail.com>
2023-11-10 10:44:42 +0100

    align code better

--- miscellaneous/benchmark.R
+++ miscellaneous/benchmark.R
@@ -1,8 +1,8 @@
 library(rbenchmark)
 
 within(rbenchmark::benchmark(
-	'test1' = { Sys.sleep(0.5) },
-  'test2' = { Sys.sleep(0.3) },
+ 'test1' = { Sys.sleep(0.5) },
+ 'test2' = { Sys.sleep(0.3) },
   replications = 5,
   columns = c("test", "replications", "elapsed", "relative")
 ), { average_min = (elapsed/replications)})
